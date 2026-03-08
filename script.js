fe64a202b4f99888ddb7a8fd5f26089ab6c41de0
duskygloom <sarkarharsh708@gmail.com>
2026-03-08 10:01:38 +0530

    Chrome extensinon + deno support

--- script.js
+++ script.js
@@ -1,46 +1,94 @@
-const chordSelector = { "wrytin.com": "div.wrytUp", "indichords.com": "div#mainData", "chordsfactory.com": "div.entry-content" }
+const chordSelector = {
+  "wrytin.com": "div.wrytUp",
+  "indichords.com": "div#mainData",
+  "chordsfactory.com": "div.entry-content",
+  "tabs.ultimate-guitar.com": "pre",
+};
 
-browser.tabs.query({ active: true, currentWindow: true })
-    .then((tabs) => {
-        const activeTab = tabs[0]
-        const pageURL = new URL(activeTab.url)
-        const basename = pageURL.hostname
+chrome.tabs.query({
+  active: true,
+  currentWindow: true,
+}).then((tabs) => {
+  const activeTab = tabs[0];
+  if (activeTab.url == null) {
+    console.log(
+      "Cannot access active tab. Try putting the active tab in host_permissions.",
+    );
+    return;
+  }
 
-        const listTag = document.getElementById("supported-websites")
+  const pageURL = new URL(activeTab.url);
+  const basename = pageURL.hostname;
+  const listTag = document.getElementById("supported-websites");
+  if (listTag == null) {
+    console.log("List tag not found. Was there any change in the popup HTML?");
+    return;
+  }
 
-        for (var key in chordSelector) {
-            var li = document.createElement("li")
-            li.textContent = key
-            listTag.appendChild(li)
+  for (const key in chordSelector) {
+    const li = document.createElement("li");
+    li.textContent = key;
+    listTag.appendChild(li);
+  }
+
+  const nameTag = document.getElementById("website-name");
+  if (nameTag == null) {
+    console.log("Name tag not found. Was there any change in the popup HTML?");
+    return;
+  }
+  nameTag.textContent = basename;
+
+  const statusTag = document.getElementById("website-status");
+  if (statusTag == null) {
+    console.log(
+      "Status tag not found. Was there any change in the popup HTML?",
+    );
+    return;
+  }
+  statusTag.textContent = `${basename in chordSelector}`;
+
+  const buttonTag = document.getElementById("simple-button");
+  if (buttonTag == null) {
+    console.log(
+      "Button tag not found. Was there any change in the popup HTML?",
+    );
+    return;
+  }
+
+  if (buttonTag instanceof HTMLButtonElement) {
+    buttonTag.disabled = !(basename in chordSelector);
+  }
+
+  if (!(basename in chordSelector)) {
+    buttonTag.onclick = null;
+    return;
+  }
+
+  buttonTag.onclick = () => {
+    chrome.scripting.executeScript({
+      target: {
+        tabId: activeTab.id,
+      },
+      func: (selector) => {
+        const element = document.querySelector(selector);
+        if (element == null) {
+          alert("No element " + selector + " found.");
+          return null;
+        } else {
+          return element.outerHTML;
         }
-
-        document.getElementById("website-name").textContent = basename
-        document.getElementById("website-status").textContent = basename in chordSelector
-        document.getElementById("simple-button").disabled = !(basename in chordSelector)
-
-        if (!(basename in chordSelector)) {
-            document.getElementById("simple-button").onclick = null
-            return
-        }
-
-        document.getElementById("simple-button").onclick = () => {
-            browser.scripting.executeScript({
-                target: {
-                    tabId: activeTab.id
-                }, func: (selector) => {
-                    const element = document.querySelector(selector)
-                    if (element == null) {
-                        alert("No element " + chordSelector[basename] + " found.")
-                        return null
-                    } else {
-                        return element.outerHTML
-                    }
-                }, args: [chordSelector[basename]]
-            }).then((results) => {
-                const outer = results[0].result
-                if (outer == null) return
-                document.body.innerHTML = outer
-            })
-        }
-    })
-
+      },
+      args: [
+        chordSelector[basename],
+      ],
+    }).then((results) => {
+      const outer = results[0].result;
+      if (outer == null) return;
+      document.body.innerHTML = outer;
+      const spans = document.getElementsByTagName("span");
+      for (let index = 0; index < spans.length; index++) {
+        spans[index].textContent = "[" + spans[index].textContent + "]";
+      }
+    });
+  };
+});
---
+++ script.ts
@@ -0,0 +1,96 @@
+import { chrome } from "chrome-types";
+
+const chordSelector = {
+  "wrytin.com": "div.wrytUp",
+  "indichords.com": "div#mainData",
+  "chordsfactory.com": "div.entry-content",
+  "tabs.ultimate-guitar.com": "pre",
+};
+
+chrome.tabs.query({ active: true, currentWindow: true })
+  .then((tabs) => {
+    const activeTab = tabs[0];
+    if (activeTab.url == null) {
+      console.log(
+        "Cannot access active tab. Try putting the active tab in host_permissions.",
+      );
+      return;
+    }
+
+    const pageURL = new URL(activeTab.url);
+    const basename = pageURL.hostname;
+
+    const listTag = document.getElementById("supported-websites");
+    if (listTag == null) {
+      console.log(
+        "List tag not found. Was there any change in the popup HTML?",
+      );
+      return;
+    }
+
+    for (const key in chordSelector) {
+      const li = document.createElement("li");
+      li.textContent = key;
+      listTag.appendChild(li);
+    }
+
+    const nameTag = document.getElementById("website-name");
+    if (nameTag == null) {
+      console.log(
+        "Name tag not found. Was there any change in the popup HTML?",
+      );
+      return;
+    }
+    nameTag.textContent = basename;
+
+    const statusTag = document.getElementById("website-status");
+    if (statusTag == null) {
+      console.log(
+        "Status tag not found. Was there any change in the popup HTML?",
+      );
+      return;
+    }
+    statusTag.textContent = `${basename in chordSelector}`;
+
+    const buttonTag = document.getElementById("simple-button");
+    if (buttonTag == null) {
+      console.log(
+        "Button tag not found. Was there any change in the popup HTML?",
+      );
+      return;
+    }
+    if (buttonTag instanceof HTMLButtonElement) {
+      buttonTag.disabled = !(basename in chordSelector);
+    }
+
+    if (!(basename in chordSelector)) {
+      buttonTag.onclick = null;
+      return;
+    }
+
+    buttonTag.onclick = () => {
+      chrome.scripting.executeScript({
+        target: {
+          tabId: activeTab.id,
+        },
+        func: (selector: string) => {
+          const element = document.querySelector(selector);
+          if (element == null) {
+            alert("No element " + selector + " found.");
+            return null;
+          } else {
+            return element.outerHTML;
+          }
+        },
+        args: [chordSelector[basename]],
+      }).then((results) => {
+        const outer = results[0].result;
+        if (outer == null) return;
+        document.body.innerHTML = outer;
+        const spans = document.getElementsByTagName("span");
+        for (let index = 0; index < spans.length; index++) {
+          spans[index].textContent = "[" + spans[index].textContent + "]";
+        }
+      });
+    };
+  });
